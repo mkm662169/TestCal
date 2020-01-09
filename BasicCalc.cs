262be95b6daeab6298fb84c8fe3ceec580cd780e
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-09 14:55:00 +0200

    Add project files.

---
+++ BasicCalc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Operations;
+namespace NewCalculator
+{
+    public class BasicCalc
+    {
+        public int solution;
+        public int Add(int a, int b)
+        {
+            solution = Addition.Add(a, b);
+            return solution;
+        }
+
+        public int Divide(int a, int b)
+        {
+            solution = Division.Divide(a, b);
+            return solution;
+        }
+    }
+}
